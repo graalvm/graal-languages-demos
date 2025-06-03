8be2559b5b827e00fa191028ecee53b807a217fe
Cedric Champeau <cedric.champeau@gmail.com>
2025-06-03 16:10:26 +0200

    Fix GraalWasm Micronaut Photon demo
    - fixed a version mismatch which caused a method not found error at runtime
    - upgrades to Micronaut 4.8.2
    - adds a default HTML page to play with effects

--- graalwasm/graalwasm-micronaut-photon/src/main/java/com/example/DemoController.java
+++ graalwasm/graalwasm-micronaut-photon/src/main/java/com/example/DemoController.java
@@ -7,12 +7,16 @@
 package com.example;
 
 import io.micronaut.context.annotation.Parameter;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Controller
 public class DemoController {
@@ -27,5 +31,10 @@ public class DemoController {
     @ExecuteOn(TaskExecutors.BLOCKING)
     public byte[] renderPhoto(@Parameter String effectName) {
         return photonService.processImage(effectName);
+    }
+
+    @Get("/")
+    public HttpResponse<?> index() throws URISyntaxException {
+        return HttpResponse.redirect(new URI("/index.html"));
     }
 }
