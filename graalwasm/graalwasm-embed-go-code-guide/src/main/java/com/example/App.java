f30cfb8bb3fe37ac43dab5dcaf2d96e3338015ec
Fabio Niephaus <fabio.niephaus@oracle.com>
2025-08-25 13:48:56 +0200

    Add comments.

--- graalwasm/graalwasm-embed-go-code-guide/src/main/java/com/example/App.java
+++ graalwasm/graalwasm-embed-go-code-guide/src/main/java/com/example/App.java
@@ -21,19 +21,24 @@ public class App {
     public static final String GO_WASM_EXEC = "/go/wasm_exec.js";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
+        // Fetch Go resources
         URL mainWasmURL = getResource(GO_MAIN_WASM);
         byte[] wasmBytes = Files.readAllBytes(Path.of(mainWasmURL.toURI()));
         URL wasmExecURL = getResource(GO_WASM_EXEC);
+        // Create a context with Wasm and JavaScript access
         try (Context context = Context.newBuilder("js", "wasm")
                 .option("js.performance", "true")
                 .option("js.text-encoding", "true")
                 .option("js.webassembly", "true")
                 .allowAllAccess(true)
                 .build()) {
+            // Install Wasm bytes and crypto polyfill in JS binding
             Value jsBindings = context.getBindings("js");
             jsBindings.putMember("wasmBytes", wasmBytes);
             jsBindings.putMember("crypto", new CryptoPolyfill());
+            // Evaluate wasm_exec.js file
             context.eval(Source.newBuilder("js", wasmExecURL).build());
+            // Instantiate the Wasm module and invoke go.run()
             context.eval("js", """
                     async function main(wasmBytes) {
                         const go = new Go();
@@ -42,6 +47,7 @@ public class App {
                     }
                     main(wasmBytes);
                     """);
+            // Access main module and interact with it through a Java interface
             GoMain goMain = jsBindings.getMember("main").as(GoMain.class);
             System.out.printf("3 + 4 = %s%n", goMain.add(3, 4));
             System.out.printf("reverseString('Hello World') = %s%n", goMain.reverseString("Hello World"));
@@ -51,7 +57,7 @@ public class App {
     private static URL getResource(String name) throws FileNotFoundException {
         URL url = App.class.getResource(name);
         if (url == null) {
-            throw new FileNotFoundException(GO_MAIN_WASM);
+            throw new FileNotFoundException(name);
         }
         return url;
     }
