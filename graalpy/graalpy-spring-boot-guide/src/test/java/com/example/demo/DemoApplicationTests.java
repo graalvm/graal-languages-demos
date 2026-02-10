30c092f61da54b4a5415f61c34d378a0f39304b0
Fabio Niephaus <fabio.niephaus@oracle.com>
2026-02-10 08:43:45 +0100

    Migrate `AutoConfigureMockMvc`.

--- graalpy/graalpy-spring-boot-guide/src/test/java/com/example/demo/DemoApplicationTests.java
+++ graalpy/graalpy-spring-boot-guide/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,8 +8,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
